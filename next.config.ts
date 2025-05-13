301325545d31f0bc4338e4980948729bfa7d82fe
Andressa Cristiny <andressasilva04178@gmail.com>
2025-05-12 23:36:30 -0300

    fix

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,4 @@
+// next.config.js ou next.config.ts
 const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig = {
@@ -5,7 +6,10 @@ const nextConfig = {
   basePath: "/portfolio",
   assetPrefix: isProd ? "/portfolio/" : "",
   trailingSlash: true,
-  images: { unoptimized: true },
+  images: {
+    unoptimized: true,
+    domains: ["s3-us-west-2.amazonaws.com", "media.licdn.com"],
+  },
 };
 
 export default nextConfig;
